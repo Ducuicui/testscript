3994d630b4d20a2658dddbde39359360affdf546
test <test@test-OptiPlex-3010.(none)>
2014-03-27 16:48:33 +0800

    update util.py

--- util.py
+++ util.py
@@ -6,56 +6,54 @@ import commands
 import time
 
 def launchApplication(app):
-	application = {'camera':'com.intel.camera22/.Camera',
-					'setting':'com.android.settings/.Settings',
-					'contact':'com.android.contacts/.activities.PeopleActivity',
-					'message':'com.android.mms/.ui.ConversationList',
-					'gallery':'com.intel.android.gallery3d/.app.Gallery',
-					'browser':'com.android.browser/.BrowserActivity',
-					'soundrecorder':'com.android.soundrecorder/.SoundRecorder'}
-	#get all support application
-	all_application = str(application.keys())
-	if app not in all_application:
-		print 'applicaiton name is wrong, only support ' + all_application
-	else:
-		#get the value of app
-		component = application.get(app)
-		#start activity
-		d.start_activity(component = component)
+    application = {'camera':'com.intel.camera22/.Camera',
+                    'setting':'com.android.settings/.Settings',
+                    'contact':'com.android.contacts/.activities.PeopleActivity',
+                    'message':'com.android.mms/.ui.ConversationList',
+                    'gallery':'com.intel.android.gallery3d/.app.Gallery',
+                    'browser':'com.android.browser/.BrowserActivity',
+                    'soundrecorder':'com.android.soundrecorder/.SoundRecorder'}
+    #get all support application
+    all_application = str(application.keys())
+    if app not in all_application:
+        print 'applicaiton name is wrong, only support ' + all_application
+    else:
+        #get the value of app
+        component = application.get(app)
+        #start activity
+        d.start_activity(component = component)
 
 def backHome():
-	for i in range(3):
-		d.press.back()
-	d.press.home()
+    for i in range(3):
+        d.press.back()
+    d.press.home()
 
 def _getWifistatus():
-	#command of check wifi status
-	Wifi_STATUS="adb shell getprop | grep init.svc.dhcpcd_wlan0"
-	wifi = commands.getoutput(Wifi_STATUS)
-	return wifi
+    #command of check wifi status
+    Wifi_STATUS="adb shell getprop | grep init.svc.dhcpcd_wlan0"
+    wifi = commands.getoutput(Wifi_STATUS)
+    return wifi
 
 def setWifiOpenClose(status):
-	if status != 'running' and status != 'stopped':
-		print 'please input running or stopped'
-	else:
-		#launch setting app
-		d.start_activity(component='com.android.settings/.Settings')
-		wifivalue = _getWifistatus()
-		i = 1
-		while status not in wifivalue and i<3:
-			print wifivalue
-			print status          
-			#click wifi switch icon 
-			d(resourceId='android:id/list',className="android.widget.ListView").child(index='1',className='android.widget.LinearLayout').child(className='android.widget.Switch').click.wait()
-			time.sleep(5)
-			wifivalue = _getWifistatus()
-			i+=1
-			
+    if status != 'running' and status != 'stopped':
+        print 'please input running or stopped'
+    else:
+        #launch setting app
+        d.start_activity(component='com.android.settings/.Settings')
+        wifivalue = _getWifistatus()
+        i = 1
+        while status not in wifivalue and i<3:
+            print wifivalue
+            print status          
+            #click wifi switch icon 
+            d(resourceId='android:id/list',className="android.widget.ListView").child(index='1',className='android.widget.LinearLayout').child(className='android.widget.Switch').click.wait()
+            time.sleep(5)
+            wifivalue = _getWifistatus()
+            i+=1
+            
 
 
 if __name__ == '__main__':
-	launchApplication('camera')
-	launchApplication('contact')
-	setWifiOpenClose('running')
-	backHome()
+    setWifiOpenClose('running')
+
 
